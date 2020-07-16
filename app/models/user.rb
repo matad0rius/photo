e3bc2de37cdb0a73cf34546977d42e6d3d2b0b9f
unknown <matad00rius@gmail.com>
2020-07-16 10:13:40 +0200

    add stripe payment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable,
          :omniauthable, :omniauth_providers => [:facebook]
+  has_one :payment
+  accepts_nested_attributes_for :payment # registration new form
 
 
   def self.from_omniauth(auth)
